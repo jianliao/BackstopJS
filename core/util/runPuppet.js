b9cea4336170d879141f8e6a434ccab166977312
Jian Liao <jianliao@adobe.com>
2021-02-18 21:53:57 -0800

    feat: support waituntil option for puppeteer

--- core/util/runPuppet.js
+++ core/util/runPuppet.js
@@ -126,8 +126,7 @@ async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenar
     // Full docs for Puppeteer waituntil: https://pptr.dev/#?product=Puppeteer&version=v2.1.1&show=api-pagewaitfornavigationoptions
     // Page goto options
     await page.goto(translateUrl(url), {
-      timeout: config.scenarioTimeOut || 1000,  // default is 1s
-      waitUntil: config.waitUntil || 'default' // default
+      waitUntil: config.waitUntil
     });
 
     await injectBackstopTools(page);
