9293771c12e4ba3dae7f3453795d7d2540962385
Jian Liao <jianliao@adobe.com>
2021-03-07 13:21:52 -0800

    remove dept react-sticky

--- compare/src/components/App.js
+++ compare/src/components/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-// ESLint
-/* eslint-disable no-unused-vars */
-import { StickyContainer } from 'react-sticky';
-
 import Header from './ecosystems/Header';
 import List from './ecosystems/List';
 import ScrubberModal from './ecosystems/ScrubberModal';
@@ -15,13 +11,13 @@ const Wrapper = styled.section`
 export default class App extends React.Component {
   render () {
     return (
-      <StickyContainer>
+      <>
         <Header />
         <Wrapper>
           <List />
         </Wrapper>
         <ScrubberModal />
-      </StickyContainer>
+      </>
     );
   }
 }
